4f6eefd8844e14c3e548f5331063efedf00747be
Your Name <you@example.com>
2023-04-16 20:26:20 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors= require("cors")
+app.use(cors())
 require("dotenv").config();
 app.use(express.json());
 const dbConfig = require("./config/dbConfig");
@@ -8,7 +9,6 @@ const dbConfig = require("./config/dbConfig");
 const usersRoute = require("./routes/usersRoute");
 const examsRoute = require("./routes/examsRoute");
 const reportsRoute = require("./routes/reportsRoute");
-app.use(cors())
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", reportsRoute);
