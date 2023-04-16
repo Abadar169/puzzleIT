8fc88b352424d08083149686b45312d2038eda99
Your Name <you@example.com>
2023-04-16 19:24:45 +0530

    export app

--- server/server.js
+++ server/server.js
@@ -1,28 +1,30 @@
-const express  = require('express');
+const express = require("express");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
-const dbConfig = require('./config/dbConfig');
+const dbConfig = require("./config/dbConfig");
 
-const usersRoute = require('./routes/usersRoute');
-const examsRoute = require('./routes/examsRoute');
-const reportsRoute = require('./routes/reportsRoute');
+const usersRoute = require("./routes/usersRoute");
+const examsRoute = require("./routes/examsRoute");
+const reportsRoute = require("./routes/reportsRoute");
 
-app.use('/api/users', usersRoute);
-app.use('/api/exams', examsRoute);
-app.use('/api/reports', reportsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/exams", examsRoute);
+app.use("/api/reports", reportsRoute);
 const port = process.env.PORT || 5000;
 
-const path = require('path');
-__dirname = path.resolve();
+// const path = require('path');
+// __dirname = path.resolve();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-    });
-}   
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+//     });
+// }
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
