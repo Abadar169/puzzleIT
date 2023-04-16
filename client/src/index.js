2810ea5a82a03cd0964ad5a76738ce10da4e170b
Your Name <you@example.com>
2023-04-16 20:20:47 +0530

    Delete dummy files

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.min.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
@@ -14,4 +13,4 @@ root.render(
   </Provider>
 );
 
-reportWebVitals();
+
